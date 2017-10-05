f9a95b2451f78d96bfc041f9b4af4669ac918119
USAMI Kenta <tadsan@zonu.me>
2017-10-06 01:48:31 +0900

    Modify routing

--- src/routing.php
+++ src/routing.php
@@ -8,8 +8,19 @@ $routing_map['show_index'] = ['GET', '/', function () {
     return view('index');
 }];
 
-$routing_map['post_feature'] = ['GET', '/feature', function () {
-    //var_dump($_POST);exit;
+$routing_map['error_notice'] = ['GET', '/error/notice/undefined_variable', function () {
+    echo $a;
+    return null;
+}];
+
+$routing_map['list_feature'] = ['GET', '/feature', function () {
+    return view('list_feature', [
+        'available_features' => Features::getAvailableValues(),
+    ]);
+}];
+
+$routing_map['post_feature'] = ['POST', '/feature', function () {
+    var_dump($_POST);exit;
 
     return view('index');
 }];
---
+++ view/list_feature.html.tpl
@@ -0,0 +1,8 @@
+{% extends "body.html.tpl" %}
+{% block content %}
+<h1>ここで差がつくエラー処理 サンプルコード</h1>
+
+<h2>機能</h2>
+{% include "_features_table.html.tpl" %}
+
+{% endblock %}
